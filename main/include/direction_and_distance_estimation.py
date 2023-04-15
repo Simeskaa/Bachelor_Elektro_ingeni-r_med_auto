5d113cc311d90f9bc762df14b95a17847c15855c
Simeskaa <81254666+Simeskaa@users.noreply.github.com>
2023-04-15 17:38:41 +0200

    rydda opp i funksjonan og gjorde an slutt klart

--- main/include/direction_and_distance_estimation.py
+++ main/include/direction_and_distance_estimation.py
@@ -90,15 +90,15 @@ class angle_cord_estimation():
 
         return list_angles, self.average_angle
 
-    def angle_2_cord_calc(self, vinkel: list):
+    def angle_2_cord_calc(self, angles: list):
         # finding start and end coordinates
         # -------------------------------------------------
         end_cords = {}
-        for i in range(len(vinkel)):
+        for i in range(len(angles)):
             x_name = f'x_{i + 1}'
             y_name = f'y_{i + 1}'
-            end_cords[x_name] = self.max_dist * np.cos(vinkel[i])
-            end_cords[y_name] = self.max_dist * np.sin(vinkel[i])
+            end_cords[x_name] = self.max_dist * np.cos(angles[i])
+            end_cords[y_name] = self.max_dist * np.sin(angles[i])
 
         start_cords = {'x_1': -self.dist_long_mic / 2, 'y_1': 0, 'x_2': 0, 'y_2': self.dist_long_mic / 2,
                        'x_3': -self.dist_long_mic / 2, 'y_3': 0, 'x_4': 0, 'y_4': self.dist_long_mic / 2,
