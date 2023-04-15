17f90341d272d7ca40007b2d7631877004c879c2
Simeskaa <81254666+Simeskaa@users.noreply.github.com>
2023-04-15 17:32:51 +0200

    rydda opp i funksjonan og gjorde an slutt klart

---
+++ main/include/test.py
@@ -0,0 +1,6 @@
+dist_long_mic = 12
+f = start_cords = {'x_1': -dist_long_mic / 2, 'y_1': 0, 'x_2': 0, 'y_2': dist_long_mic / 2,
+                       'x_3': -dist_long_mic / 2, 'y_3': 0, 'x_4': 0, 'y_4': dist_long_mic / 2,
+                       'x_5': -dist_long_mic / 2, 'y_5': 0, 'x_6': 0, 'y_6': -dist_long_mic / 2}
+a = [1, 2]
+print(len(f)/2, len(a))
