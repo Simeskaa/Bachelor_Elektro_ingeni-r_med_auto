425d4290ea0ac79f5059572d68c04e0f9b2fea28
Dawid_Win <dawid.lit32@gmail.com>
2023-04-19 16:57:33 +0200

    A version of the code where it uses pipeline and queue.

--- include/Pipeline.py
+++ include/Pipeline.py
@@ -1,31 +1,18 @@
 import logging
 import threading
+import queue
 
-class Pipeline:
-    """
-    Class to allow a single element pipeline between producer and consumer.
-    """
+class Pipeline(queue.Queue):
     def __init__(self):
-        self.message = 0
-        self.producer_lock = threading.Lock()
-        self.consumer_lock = threading.Lock()
-        self.consumer_lock.acquire()
+        super().__init__(maxsize=10)
 
     def get_message(self, name):
-        logging.debug("%s:about to acquire getlock", name)
-        self.consumer_lock.acquire()
-        logging.debug("%s:have getlock", name)
-        message = self.message
-        logging.debug("%s:about to release setlock", name)
-        self.producer_lock.release()
-        logging.debug("%s:setlock released", name)
-        return message
+        logging.debug("%s:about to get from queue", name)
+        value = self.get()
+        logging.debug("%s:got %d from queue", name, value)
+        return value
 
-    def set_message(self, message, name):
-        logging.debug("%s:about to acquire setlock", name)
-        self.producer_lock.acquire()
-        logging.debug("%s:have setlock", name)
-        self.message = message
-        logging.debug("%s:about to release getlock", name)
-        self.consumer_lock.release()
-        logging.debug("%s:getlock released", name)
+    def set_message(self, value, name):
+        logging.debug("%s:about to add %d to queue", name, value)
+        self.put(value)
+        logging.debug("%s:added %d to queue", name, value)
--- main/main.py
+++ main/main.py
@@ -14,25 +14,27 @@ buffer_size = 1024
 SENTINEL = object()
 
 
-def producer(pipeline):
+def producer(pipeline, event):
     """ Recieving a message from the network """
+    while not event.is_set():
+        inc_msg = recv()
+        logging.info("Producer got message: %s", inc_msg)
+        pipeline.set_message(inc_msg, "Producer")
 
-    inc_msg = recv()
-    logging.info("Producer got message: %s", inc_msg)
-    pipeline.set_message(inc_msg, "Producer")
-
-    """ Send a sentinel message to tell consumer we're done """
-    pipeline.set_message(SENTINEL, "Producer")
+    logging.info("Producer received EXIT event. Exiting")
 
 
-def consumer(pipeline):
-    message = None
-
-    while message is not SENTINEL:
+def consumer(pipeline, event):
+    """Saving data to a database"""
+    while not event.is_set() or not pipeline.empty():
         message = pipeline.get_message("Consumer")
-        if message is not SENTINEL:
-            logging.info("Consumer storing message: %s", message)
+        logging.info(
+            "Consumer storing message: %s  (queue size=%s)",
+            message,
+            pipeline.qsize(),
+        )
 
+    logging.info("Consumer received EXIT event. Exiting")
 
 def recv():
     msg = UDP.get_message(65508)
@@ -44,15 +46,18 @@ if __name__ == "__main__":
     formt = "%(asctime)s: %(message)s"
     logging.basicConfig(format=formt, level=logging.INFO,
                         datefmt="%H:%M:%S")
-    logging.getLogger().setLevel(logging.DEBUG)
+    # logging.getLogger().setLevel(logging.DEBUG)
 
     UDP = UDP(ip_adress="192.168.0.69", port=5004, receive_msg=True)
     pipeline = Pipeline()
-    while True:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-            executor.submit(producer, pipeline)
-            executor.submit(consumer, pipeline)
+    event = threading.Event()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(producer, pipeline, event)
+        executor.submit(consumer, pipeline, event)
 
+        time.sleep(0.1)
+        logging.info("Main: about to set event")
+        event.set()
 
 
 
